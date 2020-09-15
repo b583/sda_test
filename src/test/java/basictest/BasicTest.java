5427d59a9d7d0cbb5b03a76d49035780fd7e4e12
tszczodrowski <szczodrowski@protonmail.com>
2020-09-15 13:08:13 +0200

    Add BasicTest

---
+++ src/test/java/basictest/BasicTest.java
@@ -0,0 +1,13 @@
+package basictest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BasicTest {
+
+    @Test
+    public void basicTest() {
+        Assertions.assertTrue(true);
+    }
+
+}
