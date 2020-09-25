090b1a768244d7f96624321109a5e63c0794f889
tszczodrowski <szczodrowski@protonmail.com>
2020-09-25 10:33:35 +0200

    Make AssertionsTest exmaple less abstract

--- src/test/java/example/AssertionsTest.java
+++ src/test/java/example/AssertionsTest.java
@@ -8,12 +8,16 @@ class AssertionsTest {
     @Test
     void moreBasicAssertionsTest() {
         // remember - first expected, then actual
-        Assertions.assertEquals(1,1);
-        Assertions.assertNotEquals(1, 2);
+        Assertions.assertEquals(1, sum(0, 1));
+        Assertions.assertNotEquals(1, sum(3, -1));
 
         // self explaining
         Assertions.assertTrue(true);
         Assertions.assertFalse(false);
+    }
+
+    private int sum(int a, int b) {
+        return a + b;
     }
 
 }
