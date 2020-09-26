3faffcac05a8983927ff4d13efe24f0a43c5c218
Tomasz Szczodrowski <bdn583@gmail.com>
2020-09-26 17:55:38 +0200

    Add testArrays and testIterables

--- src/test/java/example/AssertionsTest.java
+++ src/test/java/example/AssertionsTest.java
@@ -3,6 +3,8 @@ package example;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 class AssertionsTest {
 
     @Test
@@ -18,6 +20,22 @@ class AssertionsTest {
 
     private int sum(int a, int b) {
         return a + b;
+    }
+
+    @Test
+    void testArrays() {
+        // test array content
+        final int[] arrayOne = {1, 2, 3};
+        final int[] arrayTwo = {1, 2, 3};
+        Assertions.assertArrayEquals(arrayOne, arrayTwo);
+    }
+
+    @Test
+    void testIterables() {
+        // test iterable content e.g. lists
+        final var listOne = List.of("A", "B", "C");
+        final var listTwo = List.of("A", "B", "C");
+        Assertions.assertIterableEquals(listOne, listTwo);
     }
 
 }
