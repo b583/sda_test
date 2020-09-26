c1fbabccfd368ae37bbefb8261dee047b2c98e4c
Tomasz Szczodrowski <bdn583@gmail.com>
2020-09-26 17:01:49 +0200

    Add ReferenceTest

---
+++ src/test/java/example/ReferenceTest.java
@@ -0,0 +1,29 @@
+package example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ReferenceTest {
+
+    @Test
+    void referenceTest() {
+        final var dummyOne = new DummyClass();
+        final var dummyTwo = new DummyClass();
+
+        Assertions.assertSame(dummyOne, dummyTwo);
+    }
+
+    @Test
+    void boxedValueReferenceTest() {
+        // see that test succeeds due to Integer using cache for valueOf method
+        final var int1 = Integer.valueOf(1);
+        final var int2 = Integer.valueOf(1);
+
+        Assertions.assertSame(int1, int2);
+    }
+
+    private static class DummyClass {
+
+    }
+
+}
